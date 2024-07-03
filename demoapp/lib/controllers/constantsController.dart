f663c2672eace1c0cde4448532dd82408160a1c0
Aryan Dhawan <aryansdhawan@gmail.com>
2024-07-03 02:36:22 -0400

    Converting away from GetX

--- demoapp/lib/controllers/constantsController.dart
+++
@@ -1,8 +0,0 @@
-import "package:get/get.dart";
-import "package:flutter/material.dart";
-
-class ConstantsController extends GetxController {
-
-  final Color SCAFFOLD_BACKGROUND_COLOR = const Color(0xFFF4E8C1);
-  final Color APPBAR_BACKGROUND_COLOR = const Color(0xFFDCC26D);
-}
